3d215713877baeb6b72491730ea05faa1e4fe942
afeiship <1290657123@qq.com>
2024-04-01 22:35:46 +0800

    feat: add init fmt/output file

--- bin/index.js
+++ bin/index.js
@@ -1,24 +1,26 @@
 #!/usr/bin/env node
 
-import { Command } from 'commander';
+import { Command, Option } from 'commander';
 import { createRequire } from 'module';
-// import sharp from 'sharp';
-import { globby } from 'globby';
+import sharp from 'sharp';
+import fs from 'fs';
+import path from 'path';
+import { globbySync } from 'globby';
 
 const __dirname = new URL('../', import.meta.url).pathname;
 const require = createRequire(__dirname);
 const pkg = require('./package.json');
 const program = new Command();
+const SUPPORT_FORMATS = ['jpeg', 'png', 'webp', 'gif'];
 
 program.version(pkg.version);
 program
-  .option('-f, --force', 'force to create')
-  .option('-i, --input', 'your input globs')
-  .option('-o, --output', 'your output dir')
-  .option('-s, --scale', 'scale of image(1-100)')
-  .option('-q, --quality', 'quality of image(1-100)')
-  .option('-t, --target-format', 'target format of image')
-  .option('-v, --verbose', 'show verbose log')
+  .addOption(new Option('-i, --input <string>', 'your input globs').default('*.*'))
+  .addOption(new Option('-o, --output <string>', 'your output dir').default('dist'))
+  .addOption(new Option('-s, --scale <number>', 'scale of image(1-100)').default(100).argParser(parseFloat))
+  .addOption(new Option('-q, --quality <number>', 'quality of image(1-100)').default(80).argParser(parseFloat))
+  .addOption(new Option('-f, --format <string>', 'format of image'))
+  .addOption(new Option('-v, --verbose <boolean>', 'show verbose log').default(false))
   .parse(process.argv);
 
 /**
@@ -37,9 +39,24 @@ class CliApp {
     if (verbose) console.log('📗', ...args);
   }
 
+  ensureDir() {
+    const { output } = this.opts;
+    if (!fs.existsSync(output)) {
+      fs.mkdirSync(output);
+    }
+  }
+
   run() {
-    console.log(this.args);
-    // this.log('run cli: ', __dirname, this.args, this.opts, pkg.version);
+    const { input, format, quality, wepb } = this.opts;
+    const files = globbySync(input);
+    this.ensureDir();
+    files.forEach((file) => {
+      const isSupportFormat = SUPPORT_FORMATS.includes(format);
+      const ext = path.extname(file).slice(1);
+      const fmt = isSupportFormat ? format : ext;
+      const outputFile = path.join(this.opts.output, path.basename(file)).replace(/\.\w+$/, `.${fmt}`);
+      sharp(file).toFormat(fmt, { quality }).toFile(outputFile);
+    });
   }
 }
 
